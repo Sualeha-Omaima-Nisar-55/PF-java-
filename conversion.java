be2299b966ef57ba2fdfa3b6040d60df05cc524c
Sualeha-Omaima-Nisar-55 <nazifarooqui2850@gmail.com>
2026-03-31 06:44:49 -0700

    Add files via upload

---
+++ conversion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class conversion {
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+    System.out.print("Enter an integer: ");
+    int num = sc.nextInt();
+
+    int inpnum = num;
+    int binary = 0;
+    int place = 1;
+
+    while (num > 0) {
+        int remainder = num % 2;
+        binary = binary + (remainder * place);
+        num = num / 2;
+        place = place * 10;
+    }
+    System.out.println(binary + " is binary conversion of  : " + inpnum);
+}
+}
